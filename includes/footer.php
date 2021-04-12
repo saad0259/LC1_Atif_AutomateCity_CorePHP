048e820d677e8d6909df15438cc9254e9691729c
Saad Shafiq <saadshafiq259@gmail.com>
2021-04-12 06:57:07 +0500

    added logo and some style

---
+++ includes/footer.php
@@ -0,0 +1,145 @@
+	<!-- Footer -->
+    <footer class="footer" style="background-image:url('img/map.png')">
+			<!-- Footer Top -->
+			<div class="footer-top">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-3 col-md-6 col-12">
+							<!-- Footer About -->		
+							<div class="single-widget footer-about widget">	
+								<div class="logo">
+									<div class="img-logo">
+										<a class="logo" href="index.php">
+											<img class="img-responsive" src="img/logo03_02.png" alt="logo">
+										</a>
+									</div>
+								</div>
+								<div class="footer-widget-about-description">
+									<p>Beatae vitae dicta su explicabo nemo enim ipsam voluptatem quia voluptas sitBeatae vitae sitBeatae vitae dicta suntania..</p>
+								</div>	
+								<div class="social">
+									<!-- Social Icons -->
+									<ul class="social-icons">
+										<li><a class="facebook" href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+										<li><a class="twitter" href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
+										<li><a class="linkedin" href="#" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+										<li><a class="pinterest" href="#" target="_blank"><i class="fa fa-pinterest-p"></i></a></li>
+										<li><a class="instagram" href="#" target="_blank"><i class="fa fa-instagram"></i></a></li>
+									</ul>
+								</div>
+								<div class="button"><a href="#" class="bizwheel-btn">About Us</a></div>
+							</div>		
+							<!--/ End Footer About -->		
+						</div>
+						<div class="col-lg-2 col-md-6 col-12">
+							<!-- Footer Links -->		
+							<div class="single-widget f-link widget">
+								<h3 class="widget-title">Company</h3>
+								<ul>
+									<li><a href="#">About Us</a></li>
+									<li><a href="#">Our Services</a></li>
+									<li><a href="#">Portfolio</a></li>
+									<li><a href="#">Pricing Plan</a></li>
+									<li><a href="#">Contact us</a></li>
+								</ul>
+							</div>			
+							<!--/ End Footer Links -->			
+						</div>
+						<div class="col-lg-4 col-md-6 col-12">
+							<!-- Footer News -->
+							<div class="single-widget footer-news widget">	
+								<h3 class="widget-title">Testimonials</h3>
+								<!-- Single News -->
+								<div class="single-f-news">
+									<div class="post-thumb"><a href="#"><img src="https://via.placeholder.com/70x70" alt="#"></a></div>
+									<div class="content">
+										<p class="post-meta"><time class="post-date"><i class="fa fa-clock-o"></i>April 15, 2020</time></p>
+										<h4 class="title"><a href="blog-sngle.html">They Provide you Best Creative Consulting Service</a></h4>
+									</div>
+								</div>
+								<!--/ End Single News -->
+								<!-- Single News -->
+								<div class="single-f-news">
+									<div class="post-thumb"><a href="#"><img src="https://via.placeholder.com/70x70" alt="#"></a></div>
+									<div class="content">
+										<p class="post-meta"><time class="post-date"><i class="fa fa-clock-o"></i>April 10, 2020</time></p>
+										<h4 class="title"><a href="blog-sngle.html">They Provide you Best Creative Consulting Service</a></h4>
+									</div>
+								</div>
+								<!--/ End Single News -->
+							</div>			
+							<!--/ End Footer News -->			
+						</div>
+						<div class="col-lg-3 col-md-6 col-12">	
+							<!-- Footer Contact -->		
+							<div class="single-widget footer_contact widget">	
+								<h3 class="widget-title">Contact</h3>
+								<p>Beatae vitae dicta sunt explicabo nemo enim ipsam voluptatem</p>
+								<ul class="address-widget-list">
+									<li class="footer-mobile-number"><i class="fa fa-phone"></i>+(92) 305 6558626</li>
+									<li class="footer-mobile-number"><i class="fa fa-envelope"></i>info@yoursite.com</li>
+									<li class="footer-mobile-number"><i class="fa fa-map-marker"></i>CA, United States</li>
+								</ul>
+							</div>		
+							<!--/ End Footer Contact -->						
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-12">
+							<!-- Footer Newsletter -->
+							<div class="footer-newsletter">
+								<form action="#" method="post" class="newsletter-area">
+									<input type="email" placeholder="Your email address">
+									<button type="submit">Sign Up</button>
+								</form>
+							</div>
+							<!--/ End Footer Newsletter -->
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Copyright -->
+			<div class="copyright">
+				<div class="container">
+					<div class="row">
+						<div class="col-12">
+							<div class="copyright-content">
+								<!-- Copyright Text -->
+								<p>© Copyright <a href="#">AutomateCity</a>. Design &amp; Made with ❤ By <a target="_blank" href="#">Saad.S</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--/ End Copyright -->
+		</footer>
+		
+		<!-- Jquery JS -->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery-migrate-3.0.0.js"></script>
+		<!-- Popper JS -->
+		<script src="js/popper.min.js"></script>
+		<!-- Bootstrap JS -->
+		<script src="js/bootstrap.min.js"></script>
+		<!-- Modernizr JS -->
+		<script src="js/modernizr.min.js"></script>
+		<!-- ScrollUp JS -->
+		<script src="js/scrollup.js"></script>
+		<!-- FacnyBox JS -->
+		<script src="js/jquery-fancybox.min.js"></script>
+		<!-- Cube Portfolio JS -->
+		<script src="js/cubeportfolio.min.js"></script>
+		<!-- Slick Nav JS -->
+		<script src="js/slicknav.min.js"></script>
+		<!-- Slick Nav JS -->
+		<script src="js/slicknav.min.js"></script>
+		<!-- Slick Slider JS -->
+		<script src="js/owl-carousel.min.js"></script>
+		<!-- Easing JS -->
+		<script src="js/easing.js"></script>
+		<!-- Magnipic Popup JS -->
+		<script src="js/magnific-popup.min.js"></script>
+		<!-- Active JS -->
+		<script src="js/active.js"></script>
+	</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="zxx">
+	<head>
+		<!-- Meta Tag -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name='copyright' content='pavilan'>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<!-- Title Tag  -->
+		<title>Bizwheel - Multipurpose Business & Digital Agency Html5 Template</title>
+		
+		<!-- Favicon -->
+		<link rel="icon" type="image/favicon.png" href="img/favicon.png">
+		
+		<!-- Web Font -->
+		<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+		
+		<!-- Bizwheel Plugins CSS -->
+		<link rel="stylesheet" href="css/animate.min.css">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/cubeportfolio.min.css">
+		<link rel="stylesheet" href="css/font-awesome.css">
+		<link rel="stylesheet" href="css/jquery.fancybox.min.css">
+		<link rel="stylesheet" href="css/magnific-popup.min.css">
+		<link rel="stylesheet" href="css/owl-carousel.min.css">
+		<link rel="stylesheet" href="css/slicknav.min.css">
+
+		<!-- Bizwheel Stylesheet -->  
+		<link rel="stylesheet" href="css/reset.css">
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/responsive.css">
+
+		<!-- <link rel="stylesheet" href="css/skins/skin-4.css"> -->
+	
+		
+	</head>
+	<body id="bg">
+	
+		<!-- Boxed Layout -->
+		<div id="page" class="site boxed-layout"> 
+		
+		<!-- Preloader -->
+		<div class="preeloader">
+			<div class="preloader-spinner"></div>
+		</div>
+		<!--/ End Preloader -->
+	
+		<!-- Header -->
+		<header class="header">
+			<!-- Topbar -->
+			<div class="topbar">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-8 col-12">
+							<!-- Top Contact -->
+							<div class="top-contact">
+								<div class="single-contact"><i class="fa fa-phone"></i>Phone: +(92) 305 6558626</div> 
+								<div class="single-contact"><i class="fa fa-envelope-open"></i>Email: info@yoursite.com</div>	
+								<div class="single-contact"><i class="fa fa-clock-o"></i>Opening: 08AM - 09PM</div> 
+							</div>
+							<!-- End Top Contact -->
+						</div>	
+						<div class="col-lg-4 col-12">
+							<div class="topbar-right">
+								<!-- Social Icons -->
+								<ul class="social-icons">
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+									<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+								</ul>															
+								<div class="button">
+									<a href="contact.html" class="bizwheel-btn">Get a Quote</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--/ End Topbar -->
+			<!-- Middle Header -->
+			<div class="middle-header">
+				<div class="container">
+					<div class="row">
+						<div class="col-12">
+							<div class="middle-inner">
+								<div class="row">
+									<div class="col-lg-2 col-md-3 col-12">
+										<!-- Logo -->
+										<div class="logo">
+											<!-- Image Logo -->
+											<div class="img-logo">
+												<a href="index.php">
+													<img src="img/logo03.png" alt="#">
+												</a>
+											</div>
+										</div>								
+										<div class="mobile-nav"></div>
+									</div>
+									<div class="col-lg-10 col-md-9 col-12">
+										<div class="menu-area">
+											<!-- Main Menu -->
+											<nav class="navbar navbar-expand-lg">
+												<div class="navbar-collapse">	
+													<div class="nav-inner">	
+														<div class="menu-home-menu-container">
+															<!-- Naviagiton -->
+															<ul id="nav" class="nav main-menu menu navbar-nav">
+																<li><a href="index.php">Home</a></li>
+																<li><a href="about.html">About Us</a></li>
+																<li class="icon-active"><a href="#">Coaching</a>
+																	<ul class="sub-menu">
+																		<li><a href="blog.html">FBA Proficiency</a></li>
+																		<li><a href="blog-single.html">Social Media Class</a></li>
+																	</ul>
+																</li>
+
+                                                                <li class="icon-active"><a href="#">Automation</a>
+																	<ul class="sub-menu">
+																		<li><a href="blog.html">Complete</a></li>
+																		<li><a href="blog-single.html">Partial</a></li>
+																	</ul>
+																</li>
+                                                                <li><a href="contact.html">Testimonials</a></li>
+                                                                <li><a href="contact.html">Contact Us</a></li>
+																<li><a href="contact.html">FAQ</a></li>
+															</ul>
+															<!--/ End Naviagiton -->
+														</div>
+													</div>
+												</div>
+											</nav>
+											<!--/ End Main Menu -->	
+											<!-- Right Bar -->
+											<div class="right-bar">
+												<!-- Search Bar -->
+												<ul class="right-nav">
+													<li class="top-search"><a href="#0"><i class="fa fa-search"></i></a></li>
+													<li class="bar"><a class="fa fa-bars"></a></li>
+												</ul>
+												<!--/ End Search Bar -->
+												<!-- Search Form -->
+												<div class="search-top">
+													<form action="#" class="search-form" method="get">
+														<input type="text" name="s" value="" placeholder="Search here"/>
+														<button type="submit" id="searchsubmit"><i class="fa fa-search"></i></button>
+													</form>
+												</div>
+												<!--/ End Search Form -->
+											</div>	
+											<!--/ End Right Bar -->
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--/ End Middle Header -->
+			<!-- Sidebar Popup -->
+			<div class="sidebar-popup">
+				<div class="cross">
+					<a class="btn"><i class="fa fa-close"></i></a>
+				</div>
+				<div class="single-content">
+					<h4>About AutomateCity</h4>
+					<p>Strategic Concepts for Evolving Businesses.</p>
+					<!-- Social Icons -->
+					<ul class="social">
+						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+						<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+					</ul>
+				</div>
+				<div class="single-content">
+					<h4>Important Links</h4>   
+					<ul class="links">
+						<li><a href="#">About Us</a></li>
+						<li><a href="#">Our Services</a></li>
+						<li><a href="#">Portfolio</a></li>
+						<li><a href="#">Pricing Plan</a></li>
+						<li><a href="#">Blog & News</a></li>
+						<li><a href="#">Contact us</a></li>
+					</ul>
+				</div>	
+			</div>
+			<!--/ Sidebar Popup -->	
+		</header>
+		<!--/ End Header -->
